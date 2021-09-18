4bae4a5243b2856106e09e5de5964b4bff293bd1
Aviraj Khare <aviraj.khare@aux.go-jek.com>
2021-09-18 10:05:43 +0530

    [Aviraj] Adds functionality to add videos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.FileField(upload_to='uploads/%y')
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Video
 
 # Create your views here.
+
+def index(request):
+    videos = Video.objects.all()
+    return render(request, "index.html", {"videos": videos})
